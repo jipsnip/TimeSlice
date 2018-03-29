2c969819a0d8fc6c896983e64687ba2898d7e322
nickgoldberg42 <nick.goldberg42@gmail.com>
2018-03-28 20:17:30 -0600

    insert tests

--- TimeSlice/Startup.cs
+++ TimeSlice/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -36,6 +39,39 @@ namespace TimeSlice
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.Run(async context =>
+            {
+                if (context.Request.Path == "/") // Match with something
+                {
+                    Console.Write("got here");
+                    String course = "test";
+                    int user = 10;
+
+                    SqlConnection con = new SqlConnection("data source=den1.mssql1.gear.host;initial catalog = timeslice;user id=timeslice;password=Password123!");
+                    string query = "Insert into COURSES (courseName, userId) Values ('" + course + "', '" + user + "')";
+                    SqlCommand comm = new SqlCommand();
+                    comm.Connection = con;
+                    comm.CommandType = CommandType.Text;
+                    comm.CommandText = query;
+
+                    con.Open();
+                    comm.ExecuteNonQuery();
+
+                    String project = "Conways Game of Life";
+
+                    SqlConnection con1 = new SqlConnection("data source=den1.mssql1.gear.host;initial catalog = timeslice;user id=timeslice;password=Password123!");
+                    string query1 = "Insert into PROJECTS (projectName) Values (" + project + ")";
+                    SqlCommand comm1 = new SqlCommand();
+                    comm1.Connection = con;
+                    comm1.CommandType = CommandType.Text;
+                    comm1.CommandText = query1;
+
+                    con1.Open();
+                    comm1.ExecuteNonQuery();
+                }
+                await context.Response.WriteAsync("Hello");
+            });
 
             app.UseStaticFiles();
 

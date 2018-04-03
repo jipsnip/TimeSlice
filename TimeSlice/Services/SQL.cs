b549c6a6141a87cd164acc563672fd6f27c05553
nickgoldberg42 <nick.goldberg42@gmail.com>
2018-04-02 21:08:22 -0600

    Entered most inserts and updates into the SQL class

--- TimeSlice/Services/SQL.cs
+++ TimeSlice/Services/SQL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using TimeSlice.Models;
 
 namespace TimeSlice.Services
 {
@@ -32,15 +33,15 @@ namespace TimeSlice.Services
          * methods and executes it.  This method sets the command to the query,
          * opens the connection, executes the query, and closes the connection.
          */
-        private void Execute(string q)
-        {
-            query = q;
-            comm.CommandText = query;
+        //private void Execute(string q)
+        //{
+        //    query = q;
+        //    comm.CommandText = query;
 
-            con.Open();
-            comm.ExecuteNonQuery();
-            con.Close();
-        }
+        //    con.Open();
+        //    comm.ExecuteNonQuery();
+        //    con.Close();
+        //}
 
         /*SelectAllCoursesforUser takes the userID and returns
          *All courses in which the user is enrolled in the case of a student
@@ -48,8 +49,10 @@ namespace TimeSlice.Services
         public void SelectAllCoursesForUser(string user)
         {
             string userId = user;
-            query = "SELECT DISTINCT C.courseId, C.courseName FROM USERS U LEFT JOIN GU ON U.userId = GU.userId LEFT JOIN GROUPS G ON GU.groupId = G.groupId LEFT JOIN PG ON G.groupId = PG.groupId LEFT JOIN PROJECTS P ON PG.projectId = P.projectId LEFT JOIN CProj CP ON CP.projectId = P.projectId LEFT JOIN COURSES C ON CP.courseId = C.courseId LEFT JOIN USERS Profs ON C.userId = Profs.userId WHERE (U.userId = '" + userId + "' OR C.userId = '" + userId + "') AND C.courseId IS NOT NULL";
-            Execute(query);
+            query = "SELECT DISTINCT C.courseId, C.courseName FROM USERS U LEFT JOIN GU ON U.userId = GU.userId LEFT JOIN GROUPS G ON GU.groupId = G.groupId LEFT JOIN PG ON G.groupId = PG.groupId LEFT JOIN PROJECTS P ON PG.projectId = P.projectId LEFT JOIN CProj CP ON CP.projectId = P.projectId LEFT JOIN COURSES C ON CP.courseId = C.courseId LEFT JOIN USERS Profs ON C.userId = Profs.userId WHERE (U.userId = @userId OR C.userId = @userId) AND C.courseId IS NOT NULL";
+            comm.Parameters.AddWithValue("userId", userId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
         }
         
         public void SelectAllProjectsForCourse()
@@ -72,11 +75,101 @@ namespace TimeSlice.Services
 
         }
 
+        //inserts
+        public void InsertNewCourse(String courseName, int userId)
+        {
+            string query = "Insert into COURSES (courseName, userId) Values (@courseName, @userId)";
+            comm.Parameters.AddWithValue("courseName", courseName);
+            comm.Parameters.AddWithValue("userId", userId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void InsertNewProject(String projectName)
+        {
+            string query = "Insert into PROJECTS (projectName) Values (@projectName)";
+            comm.Parameters.AddWithValue("projectName", projectName);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void InsertNewGroup(String groupName)
+        {
+            string query = "Insert into GROUPS (groupName) Values (@groupName)";
+            comm.Parameters.AddWithValue("groupName", groupName);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void InsertNewUser(String firstName, String lastName, string userName, string password, int roleId)
+        {
+            string query = "Insert into USERS (firstName, lastName, userName, password, roleId) Values (@firstName, @lastName, @userName, @password, @roleId)";
+            comm.Parameters.AddWithValue("firstName", firstName);
+            comm.Parameters.AddWithValue("lastName", lastName);
+            comm.Parameters.AddWithValue("userName", userName);
+            comm.Parameters.AddWithValue("password", password);
+            comm.Parameters.AddWithValue("roleId", roleId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void InsertTimeSlice(DateTime startTime, DateTime endTime, int pgId, string just, int guId, int cpId)
+        {
+            string sqlStartTime = startTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string sqlEndTime = endTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string query = "Insert into TIMES (startTime, endTime, pgId, justification, guId, cpId) Values (@startTime, @endTime, @pgId, @just, @guId, @cpId)";
+            comm.Parameters.AddWithValue("startTime", sqlStartTime);
+            comm.Parameters.AddWithValue("endTime", sqlEndTime);
+            comm.Parameters.AddWithValue("pgId", pgId);
+            comm.Parameters.AddWithValue("just", just);
+            comm.Parameters.AddWithValue("guId", guId);
+            comm.Parameters.AddWithValue("cpId", cpId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        //updates
+        public void UpdateCourseName(String courseName, int courseId)
+        {
+            query = "UPDATE COURSES SET courseName = @courseName WHERE courseId = @courseId";
+            comm.Parameters.AddWithValue("courseName", courseName);
+            comm.Parameters.AddWithValue("courseId", courseId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void UpdateGroupName(String groupName, int groupId)
+        {
+            query = "UPDATE COURSES SET groupName = @groupName WHERE groupId = @groupId";
+            comm.Parameters.AddWithValue("groupName", groupName);
+            comm.Parameters.AddWithValue("groupId", groupId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void UpdateYourGroup(string groupId, int userId)
+        {
+            string query = "UPDATE GU SET groupId = @groupId WHERE userId = @userId";
+            comm.Parameters.AddWithValue("groupId", groupId);
+            comm.Parameters.AddWithValue("userId", userId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
+        public void UpdateYourPassword(string password, int userId)
+        {
+            string query = "UPDATE USERS SET password = @password WHERE userId = @userId";
+            comm.Parameters.AddWithValue("password", password);
+            comm.Parameters.AddWithValue("userId", userId);
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+
         public void CheckUserLogin(string Username, string hashedPassword)
         {
             query = "SELECT * FROM USERS WHERE password=@password AND username=@username";
             comm.Parameters.AddWithValue("password", hashedPassword);
-            comm.Parameters.AddWithValue("username", user.Username);
+            comm.Parameters.AddWithValue("username", Username);
         }
     }
 }
